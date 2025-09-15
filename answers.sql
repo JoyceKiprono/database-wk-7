928d8c2e04770c464606aac2b4804d5a2b7434da
JoyceKiprono <joyronoj3@gmail.com>
2025-09-15 13:35:14 +0300

    wk7 assignment done

---
+++ answers.sql
@@ -0,0 +1,71 @@
+-- Question 1
+CREATE DATABASE ProductDB;
+USE ProductDB;
+
+CREATE TABLE ProductDetail (
+  OrderID INT,
+  CustomerName VARCHAR(100),
+  Products VARCHAR(255)
+);
+USE ProductDB;
+INSERT INTO ProductDetail (OrderID, CustomerName, Products) VALUES
+(101, 'John Doe', 'Laptop, Mouse'),
+(102, 'Jane Smith', 'Tablet, Keyboard, Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+CREATE TABLE ProductDetail_1NF (
+  OrderID INT,
+  CustomerName VARCHAR(100),
+  Product VARCHAR(100)
+);
+
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product) VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+-- Question 2
+CREATE DATABASE OrderDB;
+USE OrderDB;
+
+CREATE TABLE OrderDetails (
+  OrderID INT,
+  CustomerName VARCHAR(100),
+  Product VARCHAR(100),
+  Quantity INT,
+  PRIMARY KEY (OrderID, Product)
+);
+USE OrderDB;
+INSERT INTO OrderDetails (OrderID, CustomerName, Product, Quantity) VALUES
+(101, 'John Doe', 'Laptop', 2),
+(101, 'John Doe', 'Mouse', 1),
+(102, 'Jane Smith', 'Tablet', 3),
+(102, 'Jane Smith', 'Keyboard', 1),
+(102, 'Jane Smith', 'Mouse', 2),
+(103, 'Emily Clark', 'Phone', 1);
+
+CREATE TABLE Orders (
+  OrderID INT PRIMARY KEY,
+  CustomerName VARCHAR(100)
+);
+
+CREATE TABLE OrderItems (
+  OrderID INT,
+  Product VARCHAR(100),
+  Quantity INT,
+  PRIMARY KEY (OrderID, Product),
+  FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
